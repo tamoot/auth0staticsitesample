f87388069d0df5de525eadce7eeeed6544618815
Masahiro TAMURA <tamoot@gmail.com>
2022-11-01 21:59:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const configureClient = async () => {
   const response = await fetchAuthConfig();
   const config = await response.json();
 
-  auth0 = await createAuth0Client({
+  auth0 = await auth0.createAuth0Client({
     domain: config.domain,
     client_id: config.clientId
   });
